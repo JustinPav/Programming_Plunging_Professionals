fb55eccf1d544b028f4fc486e4fd259e2e409baf
JustinPav <145757071+JustinPav@users.noreply.github.com>
2023-10-11 11:15:02 +1100

    Update Test.m

--- Test.m
+++ Test.m
@@ -3,48 +3,52 @@
 rosshutdown
 rosinit;
 
-%% Uses joint states
-jointStateSubscriber = rossubscriber('/dobot_magician/joint_states'); % Create a ROS Subscriber to the topic joint_states
-pause(2); % Allow some time for a message to appear
-currentJointState = jointStateSubscriber.LatestMessage.Position % Get the latest message
-jointTarget = [0,0.4,0.3,0]; % Remember that the Dobot has 4 joints by default.
-
-[targetJointTrajPub,targetJointTrajMsg] = rospublisher('/dobot_magician/target_joint_states');
-trajectoryPoint = rosmessage("trajectory_msgs/JointTrajectoryPoint");
-trajectoryPoint.Positions = jointTarget;
-targetJointTrajMsg.Points = trajectoryPoint;
-
-send(targetJointTrajPub,targetJointTrajMsg);
-
 %% Uses end effector pose
-endEffectorPoseSubscriber = rossubscriber('/dobot_magician/end_effector_poses'); % Create a ROS Subscriber to the topic end_effector_poses
-pause(2); %Allow some time for MATLAB to start the subscriber
-currentEndEffectorPoseMsg = endEffectorPoseSubscriber.LatestMessage;
-% Extract the position of the end effector from the received message
-currentEndEffectorPosition = [currentEndEffectorPoseMsg.Pose.Position.X,
-                              currentEndEffectorPoseMsg.Pose.Position.Y,
-                              currentEndEffectorPoseMsg.Pose.Position.Z];
-% Extract the orientation of the end effector
-currentEndEffectorQuat = [currentEndEffectorPoseMsg.Pose.Orientation.W,
-                          currentEndEffectorPoseMsg.Pose.Orientation.X,
-                          currentEndEffectorPoseMsg.Pose.Orientation.Y,
-                          currentEndEffectorPoseMsg.Pose.Orientation.Z];
-% Convert from quaternion to euler
-[roll,pitch,yaw] = quat2eul(currentEndEffectorQuat);
 
-endEffectorPosition = [0.2,0,0.1];
-endEffectorRotation = [0,0,0];
+% posArray lets you send an array of locations for the DoBot to move
+% through
+posArray = [0.2,-0.1,0.05;
+            0.2,-0.1,0.1;
+            0.2,-0.05,0.05;
+            0.2,-0.05,0.1;
+            0.2,0,0.05;
+            0.2,0,0.1];
 
-[targetEndEffectorPub,targetEndEffectorMsg] = rospublisher('/dobot_magician/target_end_effector_pose');
+s = size(posArray);
+for i = 1:s(1)
+    endEffectorPoseSubscriber = rossubscriber('/dobot_magician/end_effector_poses'); % Create a ROS Subscriber to the topic end_effector_poses
+    pause(1); %Allow some time for MATLAB to start the subscriber
+    currentEndEffectorPoseMsg = endEffectorPoseSubscriber.LatestMessage;
+    % Extract the position of the end effector from the received message
+    currentEndEffectorPosition = [currentEndEffectorPoseMsg.Pose.Position.X,
+        currentEndEffectorPoseMsg.Pose.Position.Y,
+        currentEndEffectorPoseMsg.Pose.Position.Z];
+    % Extract the orientation of the end effector
+    currentEndEffectorQuat = [currentEndEffectorPoseMsg.Pose.Orientation.W,
+        currentEndEffectorPoseMsg.Pose.Orientation.X,
+        currentEndEffectorPoseMsg.Pose.Orientation.Y,
+        currentEndEffectorPoseMsg.Pose.Orientation.Z];
+    % Convert from quaternion to euler
+    eul = quat2eul(currentEndEffectorQuat');
+    roll = eul(1);
+    pitch = eul(2);
+    yaw = eul(3);
 
-targetEndEffectorMsg.Position.X = endEffectorPosition(1);
-targetEndEffectorMsg.Position.Y = endEffectorPosition(2);
-targetEndEffectorMsg.Position.Z = endEffectorPosition(3);
+    endEffectorPosition = posArray(i,:);
+    endEffectorRotation = [0,0,0];
 
-qua = eul2quat(endEffectorRotation);
-targetEndEffectorMsg.Orientation.W = qua(1);
-targetEndEffectorMsg.Orientation.X = qua(2);
-targetEndEffectorMsg.Orientation.Y = qua(3);
-targetEndEffectorMsg.Orientation.Z = qua(4);
+    [targetEndEffectorPub,targetEndEffectorMsg] = rospublisher('/dobot_magician/target_end_effector_pose');
 
-send(targetEndEffectorPub,targetEndEffectorMsg);
+    targetEndEffectorMsg.Position.X = endEffectorPosition(1);
+    targetEndEffectorMsg.Position.Y = endEffectorPosition(2);
+    targetEndEffectorMsg.Position.Z = endEffectorPosition(3);
+
+    qua = eul2quat(endEffectorRotation);
+    targetEndEffectorMsg.Orientation.W = qua(1);
+    targetEndEffectorMsg.Orientation.X = qua(2);
+    targetEndEffectorMsg.Orientation.Y = qua(3);
+    targetEndEffectorMsg.Orientation.Z = qua(4);
+
+    send(targetEndEffectorPub,targetEndEffectorMsg);
+    input('Press enter to continue')
+end
