4313803de8a28876eb429e9d2c87de6af9eefd91
JustinPav <145757071+JustinPav@users.noreply.github.com>
2023-10-23 17:25:46 +1100

    Update UpdateSpinnerValues.m

--- UpdateSpinnerValues.m
+++ UpdateSpinnerValues.m
@@ -1,5 +1,5 @@
 function UpdateSpinnerValues(Tr,spinnerx,spinnery,spinnerz)
-spinnerx.Value = Tr(1,4)*100;
-spinnery.Value = Tr(2,4)*100;
-spinnerz.Value = Tr(3,4)*100;
+spinnerx.Value = Tr(1,4)*1000;
+spinnery.Value = Tr(2,4)*1000;
+spinnerz.Value = Tr(3,4)*1000;
 end
