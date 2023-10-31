40a2a55f81e28a9bcbb39929f42bd98b695aa37a
PravienK <147789830+PravienK@users.noreply.github.com>
2023-10-31 11:41:34 +1100

    Add files via upload

--- Functions/setupEnvironment.m
+++ Functions/setupEnvironment.m
@@ -19,6 +19,22 @@ shelfVertices = get(shelf,'Vertices');
 transformedShelfVertices = [shelfVertices,ones(size(shelfVertices,1),1)] * transl(-0.8,-0.55,1)';
 set(shelf,'Vertices',transformedShelfVertices(:,1:3));
 
+% Add in a plunger and move it to the front left edge of the workspace
+plunger = PlaceObject('plunger.ply');
+plungerVertices = get(plunger,'Vertices');
+scalingFactor = 0.1;
+scaledPlungerVertices = plungerVertices * scalingFactor;
+transformedPlungerVertices = [scaledPlungerVertices, ones(size(scaledPlungerVertices,1),1)] * transl(-1.25,-0.6,1.15)';
+set(plunger,'Vertices',transformedPlungerVertices(:,1:3));
+
+% Add in a bleach and move it to the front left edge of the workspace
+bleach = PlaceObject('bleach.ply');
+bleachVertices = get(bleach,'Vertices');
+scalingFactor = 0.005;
+scaledBleachVertices = bleachVertices * scalingFactor;
+transformedBleachVertices = [scaledBleachVertices, ones(size(scaledBleachVertices,1),1)] * transl(-1.25,-0.75,1.05)';
+set(bleach,'Vertices',transformedBleachVertices(:,1:3));
+
 KUKA.model.base = transl(0,0.2,0);
 KUKA.model.animate(KUKA.model.getpos)
 DoBot.model.base = transl(-0.8,-0.6,1);
