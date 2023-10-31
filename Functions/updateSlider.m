762de87e8a097661f9ca17a46f7d0e084d9be23f
JustinPav <145757071+JustinPav@users.noreply.github.com>
2023-11-01 10:53:23 +1100

    Update updateSlider.m

--- Functions/updateSlider.m
+++ Functions/updateSlider.m
@@ -1,6 +1,6 @@
 function updateSlider(robot,sliderArray)
 q = robot.model.getpos;
-for i = 1:size(sliderArray)
-    sliderArray(i).Value = q(i);
+for i = 1:size(sliderArray,2)
+    sliderArray(i).Value = rad2deg(q(i));
 end
 end
